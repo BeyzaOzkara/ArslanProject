9a2b905b3eb01bf85620046893d0cca84326a214
Beyza Ozkara <beyza.ozkara@arslan.local>
2023-08-07 15:28:23 +0300

    pics

--- ArslanProject/settings.py
+++ ArslanProject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'adminlte3',
     'adminlte3_theme',
     'tabulator',
+    'treeselect',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
