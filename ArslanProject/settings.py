8b01e3f12fb3d7b86692b29a6cfa5b4576dd64b0
Beyza Ozkara <beyza.ozkara@arslan.local>
2023-08-07 15:36:50 +0300

    treeselect

--- ArslanProject/settings.py
+++ ArslanProject/settings.py
@@ -37,7 +37,6 @@ INSTALLED_APPS = [
     'adminlte3',
     'adminlte3_theme',
     'tabulator',
-    'treeselect',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
