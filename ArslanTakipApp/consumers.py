659d8fa947ae6fb46cf5c666aaf4a70a975d40ec
Beyza Ozkara <beyza.ozkara@arslan.local>
2024-04-05 14:43:45 +0300

    notif trial 2.15

--- ArslanTakipApp/consumers.py
+++ ArslanTakipApp/consumers.py
@@ -1,42 +1,41 @@
-from channels.generic.websocket import WebsocketConsumer
-from asgiref.sync import async_to_sync
+from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-class NotificationConsumer(WebsocketConsumer):
-    def connect(self):
+class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.group_name = 'notifications_group'
-        async_to_sync(self.channel_layer.group_add)(
+        await self.channel_layer.group_add(
             self.group_name,
             self.channel_name
         )
-        self.accept()
+        await self.accept()
 
-        self.send(text_data = json.dumps({
+        await self.send(text_data = json.dumps({
             'type': 'connection_established',
-            'message': str(self.group_name) + str(self.channel_name)
+            'message': 'Connection established!'
         }))
 
-    def disconnect(self, close_code):
-        async_to_sync(self.channel_layer.group_discard)(
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
             self.group_name,
             self.channel_name
         )
 
-    def receive(self, text_data):
+    async def receive(self, text_data):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
         print(message)
         # Process the message as needed
-        async_to_sync(self.channel_layer.group_send)(
+        await self.channel_layer.group_send(
             self.group_name, 
             {
-                "type": "send_notification", 
+                "type": "chat.message", 
                 "message": message
             }
         )
 
-    def notif_message(self, event):
+    async def chat_message(self, event):
         message = event['message']
         print(message)
         # Send message to WebSocket
-        self.send(text_data=json.dumps({"message": message}))
+        await self.send(text_data=json.dumps({"message": message}))
