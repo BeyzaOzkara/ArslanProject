f09ca87a4ceef6374b9e95e1212c30837a90981d
Beyza Ozkara <beyza.ozkara@arslan.local>
2024-04-06 13:40:27 +0300

    User Based Notification System 1.4

--- ArslanProject/asgi.py
+++ ArslanProject/asgi.py
@@ -6,7 +6,7 @@ It exposes the ASGI callable as a module-level variable named ``application``.
 For more information on this file, see
 https://docs.djangoproject.com/en/4.2/howto/deployment/asgi/
 """
-
+# /bin/bash -c 'source /home/arslanplc01/Env2/ArslanP/bin/activate && /
 import os
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
--- ArslanTakipApp/consumers.py
+++ ArslanTakipApp/consumers.py
@@ -4,6 +4,42 @@ from .models import Notification
 
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
+        self.group_name = 'notifications_group'
+        await self.channel_layer.group_add(
+            self.group_name,
+            self.channel_name
+        )
+        await self.accept()
+        await self.send(text_data = json.dumps({
+            'type': 'connection_established',
+            'message': 'Connection established!'
+        }))
+
+    async def disconnect(self, close_code):
+        await self.channel_layer.group_discard(
+            self.group_name,
+            self.channel_name
+        )
+
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json["message"]
+        # Process the message as needed
+        await self.channel_layer.group_send(
+            self.group_name, 
+            {
+                "type": "chat.message", 
+                "message": message
+            }
+        )
+
+    async def chat_message(self, event):
+        message = event['message']
+        # Send message to WebSocket
+        await self.send(text_data=json.dumps({"message": message}))
+
+# class NotificationConsumer(AsyncWebsocketConsumer):
+    async def connect(self):
         self.user = self.scope['user']
         self.room_name = f'user_{self.user.id}_notifications'
         self.room_group_name = f'notifications_{self.user.id}'
