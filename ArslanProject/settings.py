a958a1ab945ea570931ebed0786c4b220bf17184
Beyza Ozkara <beyza.ozkara@arslan.local>
2024-04-06 15:44:10 +0300

    User Based Notification System 1.14

--- ArslanProject/settings.py
+++ ArslanProject/settings.py
@@ -89,7 +89,7 @@ CHANNEL_LAYERS = {
     "default": {
         "BACKEND": "channels_redis.core.RedisChannelLayer",
         "CONFIG": {
-            "hosts": [("192.168.150.230", 6379)],
+            "hosts": [("0.0.0.0", 6379)],
         },
     },
 }
