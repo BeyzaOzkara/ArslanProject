7a7bae09d1da97f7a806d85152e8876f953bc23d
Beyza Ozkara <beyza.ozkara@arslan.local>
2024-04-06 16:15:38 +0300

    User Based Notification System 1.17

--- ArslanTakipApp/routing.py
+++ ArslanTakipApp/routing.py
@@ -3,14 +3,6 @@ from channels.auth import AuthMiddlewareStack
 from django.urls import re_path
 from .consumers import NotificationConsumer
 
-# application = ProtocolTypeRouter({
-#     'websocket': AuthMiddlewareStack(
-#         URLRouter([
-#             re_path('ws/notifications/', NotificationConsumer.as_asgi()),
-#         ])
-#     ),
-# })
-
 websocket_urlpatterns = [
     re_path(r"ws/notifications/", NotificationConsumer.as_asgi()),
 ]
