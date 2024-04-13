4c7408d889b9c80fe23944ce3c0c475a67e9ed6d
Beyza Ozkara <beyza.ozkara@arslan.local>
2024-04-13 15:47:51 +0300

    Notification 1.5

--- ArslanProject/asgi.py
+++ ArslanProject/asgi.py
@@ -11,6 +11,7 @@ from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.security.websocket import AllowedHostsOriginValidator
 from channels.routing import ProtocolTypeRouter, URLRouter
+from channels.layers import get_channel_layer
 from ArslanTakipApp.routing import websocket_urlpatterns
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ArslanProject.settings')
@@ -26,3 +27,5 @@ application = ProtocolTypeRouter(
         )
     }
 )
+
+channel_layer = get_channel_layer()
