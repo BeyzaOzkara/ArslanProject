b4f1e2e8c29298ed2ee5ca3a650230e8e42a9885
Beyza Ozkara <beyza.ozkara@arslan.local>
2024-04-15 09:03:32 +0300

    notifications unread comment

--- ArslanTakipApp/consumers.py
+++ ArslanTakipApp/consumers.py
@@ -1,42 +1,6 @@
 from channels.generic.websocket import AsyncWebsocketConsumer
 import json
 
-# class NotificationConsumer(AsyncWebsocketConsumer):
-#     async def connect(self):
-#         self.group_name = 'notifications_group'
-#         await self.channel_layer.group_add(
-#             self.group_name,
-#             self.channel_name
-#         )
-#         await self.accept()
-#         await self.send(text_data = json.dumps({
-#             'type': 'connection_established',
-#             'message': 'Connection established!'
-#         }))
-
-#     async def disconnect(self, close_code):
-#         await self.channel_layer.group_discard(
-#             self.group_name,
-#             self.channel_name
-#         )
-
-#     async def receive(self, text_data):
-#         text_data_json = json.loads(text_data)
-#         message = text_data_json["message"]
-#         # Process the message as needed
-#         await self.channel_layer.group_send(
-#             self.group_name, 
-#             {
-#                 "type": "chat.message", 
-#                 "message": message
-#             }
-#         )
-
-#     async def chat_message(self, event):
-#         message = event['message']
-#         # Send message to WebSocket
-#         await self.send(text_data=json.dumps({"message": message}))
-
 class NotificationConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         self.user = self.scope["user"]
@@ -51,7 +15,7 @@ class NotificationConsumer(AsyncWebsocketConsumer):
             'type': 'connection_established',
             'message': str(self.user) + ' ' + str(self.user.id),
         }))
-        await self.send_unread_notifications()
+        # await self.send_unread_notifications()
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
