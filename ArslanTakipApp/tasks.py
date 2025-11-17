06c8383b384b053a3b552ca633b3fc01d9b8f3cb
BeyzaOzkara <beyzaozkara10@gmail.com>
2025-11-17 09:19:10 +0300

    celery tasks

--- ArslanTakipApp/tasks.py
+++ ArslanTakipApp/tasks.py
@@ -4,7 +4,10 @@ from .uretim_raporu import check_new_rapor
 from .die_update import check_new_dies, check_die_deletes
 from .reports import send_report_email_for_all
 from .utilities.test_report import send_daily_test_report_for_all, send_new_dies_without_orders_report
+import logging
+from django.utils import timezone
 
+logger = logging.getLogger(__name__)
 
 @shared_task
 def start_email_listener():
@@ -29,5 +32,18 @@ def start_report_listener_for_():
 
 @shared_task
 def start_test_report_listener():
-    send_daily_test_report_for_all()
-    send_new_dies_without_orders_report()
+    logger.info(">>> start_test_report_listener tetiklendi: %s", timezone.now())
+    # send_daily_test_report_for_all()
+    # send_new_dies_without_orders_report()
+    try:
+        send_daily_test_report_for_all()
+        logger.info(">>> daily_test başarıyla bitti")
+    except Exception as e:
+        logger.exception("!!! daily_test hata verdi: %s", e)
+        raise
+    try:
+        send_new_dies_without_orders_report()
+        logger.info(">>> new_dies başarıyla bitti")
+    except Exception as e:
+        logger.exception("!!! new_dies hata verdi: %s", e)
+        raise
