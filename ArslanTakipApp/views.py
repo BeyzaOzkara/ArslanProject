f8c2d8d610e2ae1cf1b670a5ba8f5c6174e76c4d
BeyzaOzkara <beyzaozkara10@gmail.com>
2025-11-19 10:44:32 +0300

    mail problem solved

--- ArslanTakipApp/views.py
+++ ArslanTakipApp/views.py
@@ -183,7 +183,6 @@ def hareketSave(dieList, lRec, dieTo, request):
 @permission_required("ArslanTakipApp.view_location") #izin yoksa login sayfasına yönlendiriyor
 @login_required #user must be logged in
 def location(request):
-    send_daily_test_report_for_all()
     loc = get_objects_for_user(request.user, "ArslanTakipApp.dg_view_location", klass=Location) #Location.objects.all()
     loc_list = list(loc.values().order_by('id'))
     # Create a dictionary for O(1) lookups
