a5bcf3110b36ae0a5d70bd604d39cc2292be8f3d
BeyzaOzkara <beyzaozkara10@gmail.com>
2025-12-04 13:56:06 +0300

    change date for mail

--- ArslanProject/settings.py
+++ ArslanProject/settings.py
@@ -237,7 +237,7 @@ CELERY_BEAT_SCHEDULE = {
     # },
     'send-test-email': {
         'task': 'ArslanTakipApp.tasks.start_test_report_listener', # test_report send_daily_test_report_for_all ve send_new_dies_without_orders_report
-        'schedule': crontab(minute=30, hour='13'),
+        'schedule': crontab(minute=0, hour='7'),
     },
     'send-dies-without-orders-email': {
         'task': 'ArslanTakipApp.tasks.start_new_dies_without_orders_report_listener', # test_report send_daily_test_report_for_all ve send_new_dies_without_orders_report
